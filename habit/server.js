1f3cbd7dd40d15e155c1d0363c44bc5c37ac532a
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-01-01 13:48:51 -0500

    Connect to  MongoDb

--- habit/server.js
+++ habit/server.js
@@ -13,6 +13,22 @@ app.get("/", (req, res) => {
 })
 
 
+
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
+
+require('dotenv').config();
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+
 app.listen(3100, () => {
     console.log("We are going on the port")
 })
